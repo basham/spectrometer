7410cc4b5fe2dd1ecdadbaf6a201a6309cabf0a0
Chris Basham <chris@bash.am>
2015-01-28 12:37:45 -0500

    Remove scale generator using Lab method

--- index.js
+++ index.js
@@ -38,40 +38,6 @@ function colorScale(color, range, steps) {
   return s;
 }
 
-function colorScale2(color, range, steps) {
-  var s = {};
-  range = range || 1000;
-  steps = steps || 20;
-
-  var lab = chroma(color).lab();
-
-  // Exclude the first (black) and last (white).
-  for( var i = 1; i < steps; i++ ) {
-    var lum = i / steps;
-    var name = colorName(lum, range, steps).toString();
-    var hex = chroma.lab(100 - (lum * 100), lab[1], lab[2]).hex();
-    s[name] = {
-      name: name,
-      lum: lum,
-      hex: hex
-    };
-  }
-
-  var lum = lab[0];
-  lum = (100 - lum) / 100;
-  var name = colorName(lum, range, steps).toString();
-
-  // Override the closest matching derivative color with the source color.
-  s[name] = {
-    name: name,
-    lum: lum,
-    hex: chroma(color).hex()
-  };
-
-  return s;
-}
-
-
 var source = {
   cream: '#E1D8B7',
   crimson: '#7D110C',
